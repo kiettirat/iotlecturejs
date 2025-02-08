65f76455882591569b3638a7ae1305981f838dc9
kiettirat <gh7130539@gmail.com>
2025-02-08 20:28:49 +0700

    V.3 เพิ่มNodeKm01-02

---
+++ nodekm/km01.js
@@ -0,0 +1,19 @@
+//Data Type ชนิดข้อมูล
+//String
+console.log('Hello....'+111 + false)
+console.log("Hello"+111 + true)
+//Alt+9+6
+//Alt+1+2+6
+console.log(`Hello` +111+(10+20)+true)
+//Number
+console.log(234)
+console.log(234.234234)
+//boolean
+console.log(true)
+console.log(false)
+//Array
+console.log([1,2,3])
+//odject
+console.log({name: 'game',age:20 })
+//Expression
+console.log(10+20);
